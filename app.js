83b585d70a63f2b5bcbe3b5b588b45305a7d382d
MasahiKon <konnonorth@gmail.com>
2023-01-24 12:23:49 -0800

    done

--- app.js
+++ app.js
@@ -1,70 +1,70 @@
-//Selecting individual elements
-//-- getElementById
+const addInput = document.querySelector("#add-input");
+const addBtn = document.querySelector("#add-btn");
+const list = document.querySelector("#list");
 
-const list = document.getElementById("list")
-// console.log(list)
-// list.textContent = "Hello World!!!!!"
-// list.textContent = "<h1>Hello World</h1>"
-// list.innerHTML = "<h1>Hello World</h1>"
+addBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    if(addInput.value !== "") {
+        console.log(addInput.value);
 
-//--querySelector
+        const liItem = document.createElement("li");
+        liItem.innerHTML = `
+            <p>${addInput.value}</p>
+            <p>
+                <i class="fa fa-pencil-square-o"></i>
+                <i class="fa fa-times"></i>
+            </p>
+            <input type="text" class="edit-note">
+        `
 
-const el = document.querySelector("ul li:nth-child(2)")
-const icon = el.querySelector("p .fa-times")
-icon.style.color = "yellow"
+        const editBtn = liItem.querySelector(".fa-pencil-square-o");
+        const deleteBtn = liItem.querySelector(".fa-times");
+        editBtn.addEventListener("click", () => {
+    
+            if(liItem.querySelector("input").style.display === "" || liItem.querySelector("input").style.display === "none") {
+                liItem.querySelector("input").style.display = "block";
+            } else {
+                liItem.querySelector("input").style.display = "none";
+                liItem.querySelector("input").value = "";
+            }
+            
+        });
+    
+        deleteBtn.addEventListener("click", () => {
+            list.removeChild(liItem);
+        });
+    
+        liItem.querySelector("input").addEventListener("change", (e) => {
+            liItem.querySelector("p").innerText = e.target.value;
+        });
 
-//-- loop over getElementsByClassName
+        list.appendChild(liItem);
+    }
 
-const icons = document.getElementsByClassName("fa")
-// console.log(icons)
-
-for(let i = 0; i < icons.length; i++){
-//  console.log(icons[i])
- icons[i].style.color = "blue"
-}
-
-// DOM Styles and Classes
-const headerh2 = document.querySelector('header h2') //node
-// console.log(headerh2);
-// headerh2.className = "changeBg"
-// headerh2.className = "changeFt"
-// headerh2.className += " changeBg" //concatenation
-
-headerh2.classList.add('changeBg')
-headerh2.classList.add('changeFt')
-headerh2.classList.remove('changeBg')
-
-// Get and Set and Remove attributes
-
-const divWrapper = document.querySelector('div.wrapper')
-console.log(divWrapper.getAttribute('class'));
-divWrapper.setAttribute('style', 'background-color: coral')
-divWrapper.setAttribute('alt', 'this is a div')
-divWrapper.removeAttribute('alt')
-
-const btn = document.querySelector('#add-btn')
-const input = document.querySelector('#add-input')
-
-btn.addEventListener("click", function(event){
-    event.preventDefault()
-    // console.log("clicked!")
-    input.setAttribute("value", "Hello World")
+    addInput.value = "";
 })
 
-// DOM Navigation - traversing
-const listItem = document.getElementById('list')
-//who's your daddy?
-console.log(listItem.parentNode);
-//who's your daddy's daddy?
-console.log(listItem.parentNode.parentNode);
-// console.log(listItem.parentNode.parentNode.parentNode.parentNode.parentNode);
+for(let i = 0; i < list.children.length; i++) {
+    const liItem = list.children[i];
+    const editBtn = liItem.querySelector(".fa-pencil-square-o");
+    const deleteBtn = liItem.querySelector(".fa-times");
+    editBtn.addEventListener("click", () => {
 
-//who's your child?
-console.log(listItem.childNodes);
-console.log(listItem.children);
+        if(liItem.querySelector("input").style.display === "" || liItem.querySelector("input").style.display === "none") {
+            liItem.querySelector("input").style.display = "block";
+        } else {
+            liItem.querySelector("input").style.display = "none";
+            liItem.querySelector("input").value = "";
+        }
+        
+    });
 
-//who's yøur next sibling
-console.log(listItem.nextSibling)
-console.log(listItem.nextElementSibling)
-console.log(listItem.previousElementSibling)
+    deleteBtn.addEventListener("click", () => {
+        list.removeChild(liItem);
+    });
 
+    liItem.querySelector("input").addEventListener("change", (e) => {
+        liItem.querySelector("p").innerText = e.target.value;
+    });
+
+}
